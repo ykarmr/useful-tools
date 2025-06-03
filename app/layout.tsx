5a728232b4bd557b620b1becaa8177f5ca46073b
ykarmr <yuki.arimura0508@gmail.com>
2025-06-03 10:36:58 +0900

    feat: コメントアウトされたAppleモバイルウェブアプリのメタタグを削除し、マニフェストファイルのパスを修正

--- app/layout.tsx
+++ app/layout.tsx
@@ -92,8 +92,8 @@ export default function RootLayout({
 
         <link rel="alternate" hrefLang="x-default" href={`${baseUrl}/en`} />
         <meta name="theme-color" content="#3B82F6" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        {/* <meta name="apple-mobile-web-app-capable" content="yes" /> */}
+        {/* <meta name="apple-mobile-web-app-status-bar-style" content="default" /> */}
 
         <meta
           name="apple-mobile-web-app-title"
@@ -117,7 +117,7 @@ export default function RootLayout({
           sizes="16x16"
           href="/favicon-16x16.png"
         /> */}
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/manifest.webmanifest" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
